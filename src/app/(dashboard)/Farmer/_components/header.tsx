de9fc38beb5af1bf793db1249d68e801b646b6ab
EliaShukrani <eliamuyizere36@gmail.com>
2025-08-19 11:51:12 +0200

    Improved header and farmer page

--- src/app/(dashboard)/Farmer/_components/header.tsx
+++ src/app/(dashboard)/Farmer/_components/header.tsx
@@ -4,21 +4,19 @@ import { Bell } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-interface DashboardHeaderProps {
-  onNotificationClick: () => void
-}
 
-export default function DashboardHeader({ onNotificationClick }: DashboardHeaderProps) {
+
+export default function DashboardHeader() {
   return (
     <header className="bg-white border-b border-gray-200 px-6 py-8 items-center pl-28 pr-18 justify-between ">
       <div className="flex items-center justify-between">
         <div>
           <h1 className="text-2xl font-bold text-gray-900">Farmer Dashboard</h1>
-          <p className="text-gray-600">Welcome back, Sarah! Here what happening with your products.</p>
+          <p className="text-gray-600">Welcome back, Sosten! Here what happening with your products.</p>
         </div>
 
         <div className="flex items-center gap-4">
-          <Button variant="ghost" size="icon" className="relative" onClick={onNotificationClick}>
+          <Button variant="ghost" size="icon" className="relative" >
             <Bell className="w-5 h-5" />
             <span className="absolute -top-1 -right-1 w-3 h-3 bg-red-500 rounded-full text-xs"></span>
           </Button>
